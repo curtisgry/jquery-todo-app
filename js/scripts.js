06f0d86b94c96cf0cdf0b3615383e6821f1669dc
Curtis Gray <87861225+curtisgry@users.noreply.github.com>
2022-02-25 09:35:44 -0500

    change to double click event for cross out

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@
         const addButton = $('#button');
         const list = $('#list');
 
+        // Makes items in list moveable
         list.sortable();
 
         let inputValue;
@@ -19,16 +20,21 @@
                 inputValue = input.val();
                 const newItem = $(`<li>${inputValue} <button class="delete-button">X</button></li>`);
 
-                newItem.on('click', function () {
+                // Cross out item event when double clicked
+                newItem.on('dblclick', function () {
                         toggleStrike($(this));
                 });
 
+                // Add in new item on list
                 list.append(newItem);
 
                 // Delete item event for X buttons
                 $('.delete-button').on('click', function () {
                         deleteToDoItem($(this));
                 });
+
+                // Clear input elements value
+                input.val('');
         }
 
         addButton.on('click', addToDoItem);
